15b332ceb42c3d78bd5ab269f961fc55b3e64433
Aviv Ruben Cukierman <acukierm@corn31.stanford.edu>
2015-12-08 19:13:05 -0800

    Ran over a bunch of different values of x

--- Code/SGD_script.sh
+++ Code/SGD_script.sh
@@ -1,15 +1,28 @@
-#/bin/bash
+#!/bin/bash
 
-for x in 1 
-do
-  for e in 80
-  do
-    for i in 10
-    do
-      echo $i
-      echo $e
-      echo $x
-      python SGD.py -e $e -i $i -x $x
-    done
-  done
-done    
+#run this with qsub script.sh
+
+#use current directory
+#$ -cwd
+
+#name job
+#$ -N SGD 
+
+#allow use of environment variables, like $HOME
+#$ -V
+
+#range of indices. 0 can't be an index
+#$ -t 1-10
+
+#max number of concurrent jobs (be a good citizen). Current Stanford limit is 3000
+#$ -tc 1000
+
+#combine output and error into output file, to reduce clutter
+#$ -j y
+
+INDEX=$((SGE_TASK_ID-1))
+
+ARRAY=(0.01 0.02 0.05 0.1 0.2 0.5 1.0 2.0 5.0 10.0)
+python SGD.py -e 200 -i 10 -x ${ARRAY[$INDEX]}
+
+#echo $INDEX
